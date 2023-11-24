2744ca3a2d640f86d8760110cac798ea4ebeda46
Divyesh Korat <koratdivyesh92@gmail.com>
2023-11-23 23:11:38 -0500

    create separate function for separate task

--- Car Wash/car_wash.c
+++ Car Wash/car_wash.c
@@ -3,37 +3,43 @@
 #include <time.h>
 #include <stdbool.h>
 
-int main(){
+time_t t;   
+bool found = false;
+int random_number;
+int list[5];
 
-    time_t t;
+bool checkNumber(int randNum, int cnt){
+    for(int i=0; i<cnt; i++){
+        if(list[i] == random_number){
+            printf("found\n");
+            return true;
+        }
+    }
+    return false;
+}
 
-    bool found = false;
-
-    int random_number;
-    int count = 0;
-    int list[5];
-
-    srand((unsigned) time(&t));
-
-    while(count < 5){
+int generateNumber(int count){
+    int j = 0;
+    while(j < count){
         found = false;
         random_number = rand() % 20;
         printf("genereated number : %d\n", random_number);
-                
-        for(int i=0; i<5; i++){
-            if(list[i] == random_number){
-                // printf("found\n");
-                found = true;
-                break;
-            }
-        }
+
+        found = checkNumber(random_number, count);
 
         if(!found){
-            list[count] = random_number;
-            count++;
+            list[j] = random_number;
+            printf("save list[%d]:%d\n", j, list[j]);
+            j++;
         }
-        
     }
+}
+
+int main(){
+    
+    srand((unsigned) time(&t));   
+
+    generateNumber(5);
 
     for(int i=0; i<5; i++){
         printf("list[%d] : %d\n", i, list[i]);
