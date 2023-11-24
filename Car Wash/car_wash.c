c034d959ca8b08da408cbe24c8b2e928d0b5eb02
Divyesh Korat <koratdivyesh92@gmail.com>
2023-11-23 22:35:38 -0500

    5 different number generated

--- Car Wash/car_wash.c
+++ Car Wash/car_wash.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-int main () {
-   int i, n;
-   time_t t;
-   
-   n = 5;
-   
-   /* Intializes random number generator */
-   srand((unsigned) time(&t));
+int main(){
 
-   /* Print 5 random numbers from 0 to 49 */
-   for( i = 0 ; i < n ; i++ ) {
-      printf("%d\n", rand() % 50);
-   }
-   
-   return(0);
+    time_t t;
+
+    bool found = false;
+
+    int random_number;
+    int count = 0;
+    int list[5];
+
+    srand((unsigned) time(&t));
+
+    while(count < 5){
+        found = false;
+        random_number = rand() % 20;
+        printf("genereated number : %d\n", random_number);
+                
+        for(int i=0; i<5; i++){
+            if(list[i] == random_number){
+                // printf("found\n");
+                found = true;
+                break;
+            }
+        }
+
+        if(!found){
+            list[count] = random_number;
+            count++;
+        }
+        
+    }
+
+    for(int i=0; i<5; i++){
+        printf("list[%d] : %d\n", i, list[i]);
+    }
+
 }
